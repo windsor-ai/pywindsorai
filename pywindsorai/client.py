2307501a4eb052e0a1619c66ef5227f32146cc0e
Enes Ahmeti <sundley.12345@gmail.com>
2022-10-24 11:06:22 +0300

    improved docstrings

--- pywindsorai/client.py
+++ pywindsorai/client.py
@@ -43,6 +43,15 @@ class Client(object):
         Internal helper for handling API responses from the windsor.ai server.
         Raises the appropriate exceptions when necessary; otherwise, returns the
         response.
+
+        Parameters
+        ----------
+        response : requests.Response Object
+
+        Returns
+        -------
+        dictionary
+            Dictionary of the response object.
         """
 
         # Any windsor.ai specific error handling can be done here
@@ -67,27 +76,56 @@ class Client(object):
 
     @property
     def list_connectors(self):
+        """
+        Fetch list of all possible connectors.
+
+        This function returns all the available connectors from Windsor.ai
+        platform. The list can be found in the following website:
+        https://windsor.ai/connector/all/
+
+        Parameters
+        ----------
+
+        Returns
+        -------
+        list
+            List of strings for all available connectors.
+        """
         return self._get(path="list_connectors", params={})
 
     def connectors(self, connector="all", **params):
         """
+        Fetch data from Windsor.ai
+
         Get data from one of the available windsor connectors.
         https://connectors.windsor.ai/#operations-default-get_connector
 
-        Example usage:
-            from pywindsorai.enums import LAST_7D
-            from pywindsorai.fields import FIELD_SOURCE, FIELD_CAMPAIGN, FIELD_CLICKS
+        Parameters
+        ----------
+        connector : str
+            A string with the preferred connector. Default - "all"
+        date_preset : str
+            It's recommended that you use provided enums, instead of a
+            hardcoded string. Example: LAST_7D
+        fields : list
+            List of fields. It's recommended that you use FIELD_* enums,
+            instead of hardcoded strings.
+            Example: [FIELD_SOURCE, FIELD_CAMPAIGN, FIELD_CLICKS].
 
-            client = Client(api_key)
-            orders = client.connectors(date_preset=LAST_7D, fields=[FIELD_SOURCE, FIELD_CAMPAIGN, FIELD_CLICKS])
+        Returns
+        -------
+        dictionary
+            Dictionary with the available data. In case of error, it returns
+            the error message.
 
-        :param params:
-            connector (str): A string with the preferred connector. Default - "all"
-            date_preset (str) - It's recommended that you use provided enums, instead of a hardcoded string. Example: LAST_7D
-            fields (list) - List of fields. It's recommended that you use FIELD_* enums, instead of hardcoded strings. Example: [FIELD_SOURCE, FIELD_CAMPAIGN, FIELD_CLICKS].
-        :return:
+        Examples
+        --------
+        >>> from pywindsorai.enums import LAST_7D
+        >>> from pywindsorai.fields import FIELD_SOURCE, FIELD_CAMPAIGN, FIELD_CLICKS
+
+        >>> client = Client(api_key)
+        >>> orders = client.connectors(date_preset=LAST_7D, fields=[FIELD_SOURCE, FIELD_CAMPAIGN, FIELD_CLICKS])
         """
-
         # Python interface is a list. Whereas the API expects a comma seperated string.
         params['fields'] = ','.join(params['fields'])
 
