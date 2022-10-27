c9b120243fd4f304f63aa2a4c0ddf5267ce3bb2e
Enes Ahmeti <sundley.12345@gmail.com>
2022-10-27 12:46:46 +0300

    autoincrements version

--- setup.py
+++ setup.py
@@ -1,4 +1,22 @@
 from setuptools import setup
 
+
+def update_version(filename="VERSION"):
+    with open(filename, "r+") as file:
+        version = file.read()
+        file.seek(0)
+        print(version)
+        incremented_last_digit = int(version.strip().split(".")[-1]) + 1
+        first_digits = version.strip().split(".")[0:2]
+        first_digits.append(str(incremented_last_digit))
+        new_version = ".".join(first_digits)
+        print(new_version)
+        file.write(new_version)
+        file.truncate()
+        return new_version
+
+
 if __name__ == "__main__":
-    setup()
+    setup(
+        version=update_version()
+    )
